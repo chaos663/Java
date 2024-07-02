3427a67a94c074fb9333cc59bd6bc7e34d4de621
chaos663 <ssk8571@gmail.com>
2024-07-03 01:49:58 +0900

    2024.07.03

--- Spring study-basic/core/src/main/java/honey/core/AppConfig.java
+++ Spring study-basic/core/src/main/java/honey/core/AppConfig.java
@@ -1,6 +1,9 @@
 package honey.core;
 
+import honey.core.discount.DiscountPolicy;
 import honey.core.discount.FixDiscountPolicy;
+import honey.core.discount.RateDiscountPolicy;
+import honey.core.member.MemberRepository;
 import honey.core.member.MemberService;
 import honey.core.member.MemberServiceImpl;
 import honey.core.member.MemoryMemberRepository;
@@ -9,10 +12,21 @@ import honey.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+
+        return new MemberServiceImpl(memberRepository());
+    }
+    // memberRepository는 MemoryMemberRepository 사용
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(),discountPolicy());
+    }
+    public DiscountPolicy discountPolicy(){
+        // 정액 할인
+//        return new FixDiscountPolicy();
+        //정률 할인, 금액의 10%
+        return new RateDiscountPolicy();
     }
 }
